dda0cf603eaf194e93543f36543b1d328f5a0a5b
hyhieu <hyhieu@gmail.com>
2019-05-03 12:35:35 +0000

    update

--- make-cfg.sh
+++ make-cfg.sh
@@ -3,19 +3,21 @@ TEMP_DIR=scripts/template/
 # change random seed and directory name as desired
 CFG_DIR=cfg_s1/
 SEED=1
-DATA=tiny
+DATA=mod
 
 mkdir -p scripts/"$CFG_DIR"
 # low-resource language codes
 ILS=(
-  aze
-  glg)
+  slv)
+#  glg)
 
 for i in ${!ILS[*]}; do
   IL=${ILS[$i]}
   echo $IL
-  for f in $TEMP_DIR/hs $TEMP_DIR/hs-cosine-lan_dist $TEMP_DIR/uniform-cosine-zero_one; do
-    sed "s/DATA/$DATA/g; s/SEED/$SEED/g; s/IL/$IL/g;" < $f > ${f/template/"$CFG_DIR"/}_$IL.sh 
-    chmod u+x ${f/template/"$CFG_DIR"/}_$DATA_$IL.sh 
+  echo $DATA
+  #for f in $TEMP_DIR/hs $TEMP_DIR/hs-cosine-lan_dist $TEMP_DIR/hs-cosine-lan_dist-v2 $TEMP_DIR/hs-cosine-one ; do
+  for f in $TEMP_DIR/hs $TEMP_DIR/uniform-cosine-zero_one; do
+    sed "s/DATA/$DATA/g; s/SEED/$SEED/g; s/IL/$IL/g;" < $f > ${f/template/"$CFG_DIR"/}_"$DATA"_$IL.sh 
+    chmod u+x ${f/template/"$CFG_DIR"/}_"$DATA"_$IL.sh 
   done
 done
