804070ec0093b0c578fdff48eda2a496579591a8
cindyxinyiwang <cindyxinyiwang@gmail.com>
2019-05-04 15:13:58 +0000

    update scripts

--- make-cfg.sh
+++ make-cfg.sh
@@ -3,21 +3,21 @@ TEMP_DIR=scripts/template/
 # change random seed and directory name as desired
 CFG_DIR=cfg_s1/
 SEED=1
-DATA=mod
+DATA=tal
+THRESH=18
 
 mkdir -p scripts/"$CFG_DIR"
 # low-resource language codes
 ILS=(
-  slv)
+  tam)
 #  glg)
 
 for i in ${!ILS[*]}; do
   IL=${ILS[$i]}
   echo $IL
   echo $DATA
-  #for f in $TEMP_DIR/hs $TEMP_DIR/hs-cosine-lan_dist $TEMP_DIR/hs-cosine-lan_dist-v2 $TEMP_DIR/hs-cosine-one ; do
-  for f in $TEMP_DIR/hs $TEMP_DIR/uniform-cosine-zero_one; do
-    sed "s/DATA/$DATA/g; s/SEED/$SEED/g; s/IL/$IL/g;" < $f > ${f/template/"$CFG_DIR"/}_"$DATA"_$IL.sh 
+  for f in  $TEMP_DIR/uniform-cosine-zero_one $TEMP_DIR/uniform-cosine-one $TEMP_DIR/uniform $TEMP_DIR/hs $TEMP_DIR/hs-cosine-lan_dist $TEMP_DIR/hs-cosine-one $TEMP_DIR/hs-cosine-zero_one; do
+    sed "s/THRESH/$THRESH/g; s/DATA/$DATA/g; s/SEED/$SEED/g; s/IL/$IL/g;" < $f > ${f/template/"$CFG_DIR"/}_"$DATA"_$IL.sh 
     chmod u+x ${f/template/"$CFG_DIR"/}_"$DATA"_$IL.sh 
   done
 done
